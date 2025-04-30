88516f6ae21e3fa6bc1abf6f277caf61f53c398f
WJX2001 <1758807220@qq.com>
2025-04-30 08:51:02 +0800

    feat: Build a monorepo workspace

---
+++ packages/data/constants.ts
@@ -0,0 +1,9 @@
+
+import {HELLO_WORLD} from '@togi/config'
+
+// URLS
+export const STATIC_ASSETS_URL = "https://static.hey.xyz";
+export const STATIC_IMAGES_URL = `${STATIC_ASSETS_URL}/images`;
+
+
+console.log(HELLO_WORLD)
